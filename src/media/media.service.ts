559463be9061c61023f39142174f5ac90d4e3846
Sahl Khalifa <sahl.abdelrahman@gmail.com>
2021-06-20 15:12:22 +0200

    add media service

--- src/media/media.service.ts
+++ src/media/media.service.ts
@@ -1,8 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { MediaRepository } from './entities/media.repository';
 
 @Injectable()
 export class MediaService {
-  addFileInDB(file: Express.Multer.File) {
-    return {};
+  constructor(private mediaRepository: MediaRepository) {}
+  async addFileInDB(file: Express.Multer.File): Promise<void> {
+    // check file
+    if (!file) {
+      throw new BadRequestException('File not found');
+    }
+
+    // add record for the file in db
+    await this.mediaRepository.addFile(file);
   }
 }
