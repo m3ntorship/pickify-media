dd2e0e526d6dde2080b39ab37821a1de04bafb77
Sahl Khalifa <sahl.abdelrahman@gmail.com>
2021-06-20 15:31:23 +0200

    send response using nest way

--- src/media/media.controller.ts
+++ src/media/media.controller.ts
@@ -2,15 +2,12 @@ import {
   Body,
   Controller,
   Post,
-  Res,
   UploadedFile,
   UseInterceptors,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
-import { Response } from 'express';
 import { MediaUploadDTO } from './dto/mediaUpload.dto';
 import { MediaService } from './media.service';
-// import { resolve } from 'path';
 
 @Controller('media')
 export class MediaController {
@@ -20,12 +17,11 @@ export class MediaController {
   async uploadFile(
     @Body() mediaUploadDto: MediaUploadDTO,
     @UploadedFile() file: Express.Multer.File,
-    @Res() response: Response,
   ) {
     // add file data to db
     await this.mediaService.addFileInDB(file);
 
     // send ack response
-    response.status(201).json({ ack: true });
+    return { ack: true };
   }
 }
