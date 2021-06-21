af8732b76cc94bdb5b539be341cb06e844aceec4
Sahl Khalifa <sahl.abdelrahman@gmail.com>
2021-06-21 18:52:36 +0200

    configure media module to connect to rabbitMQ broker

--- src/media/media.module.ts
+++ src/media/media.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { MulterModule } from '@nestjs/platform-express';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MulterConfigService } from 'src/config/multer';
@@ -11,6 +13,27 @@ import { MediaService } from './media.service';
   imports: [
     TypeOrmModule.forFeature([MediaRepository]),
     MulterModule.registerAsync({ useClass: MulterConfigService }),
+    ClientsModule.registerAsync([
+      {
+        name: 'MEDIA_SERVICE',
+        imports: [ConfigModule],
+        useFactory: async (configService: ConfigService) => ({
+          name: 'MEDIA_SERVICE',
+          transport: Transport.RMQ,
+          options: {
+            urls: [configService.get('rabbitURL') as string],
+            queue: 'media_queue',
+            // to ensure the message will not be deleted until a consumer sends an ack
+            noAck: false,
+            queueOptions: {
+              // the queue will survive broker restarts
+              durable: true,
+            },
+          },
+        }),
+        inject: [ConfigService],
+      },
+    ]),
   ],
   controllers: [MediaController],
   providers: [MediaService],
