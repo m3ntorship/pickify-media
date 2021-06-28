66b4bb57fabce6ab4bd35c519f7e2ecba9c81e1b
Sahl Khalifa <sahl.abdelrahman@gmail.com>
2021-06-28 17:03:56 +0200

    fix: validation

--- src/media/dto/mediaUpload.dto.ts
+++ src/media/dto/mediaUpload.dto.ts
@@ -1,5 +1,5 @@
 import { IsIn, IsUUID } from 'class-validator';
-import { entityType } from 'src/shared/enums/entityType.enum';
+import { entityType } from '../../shared/enums/entityType.enum';
 
 export class MediaUploadDTO {
   @IsIn([entityType.OPTION, entityType.POST, entityType.OPTION_GROUP])
--- src/media/media.controller.ts
+++ src/media/media.controller.ts
@@ -3,14 +3,20 @@ import {
   Controller,
   Post,
   UploadedFile,
+  UseFilters,
   UseInterceptors,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
-import type { MediaUploadDTO } from './dto/mediaUpload.dto';
+import { ValidationExceptionFilter } from '../shared/exception-filters/validation-exception.filter';
+import { MediaUploadDTO } from './dto/mediaUpload.dto';
 import type { uploadFIle as IuploadFile } from './interface/uplaodFile.interface';
 import { MediaService } from './media.service';
-
+import * as winston from 'winston';
+import { winstonLoggerOptions } from '../logging/winston.options';
 @Controller('media')
+@UseFilters(
+  new ValidationExceptionFilter(winston.createLogger(winstonLoggerOptions)),
+)
 export class MediaController {
   constructor(private mediaService: MediaService) {}
   @Post()
@@ -20,7 +26,6 @@ export class MediaController {
     @UploadedFile() file: Express.Multer.File,
   ): Promise<IuploadFile> {
     // add file data to db and send message once it is done
-
     await this.mediaService.uploadService(file, mediaUploadDto);
 
     // send ack response
