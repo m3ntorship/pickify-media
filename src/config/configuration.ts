7eabcf7c492f4a5e350962df7265e93b90e242c1
Ahmed Ayoub <great.nile@gmail.com>
2021-07-06 15:49:14 +0200

    add missing port

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,5 +1,5 @@
 export default () => ({
-  port: parseInt(process.env.PORT, 10),
+  port: parseInt(process.env.PORT, 10) || 3000,
   rabbitURL: process.env.RABBITMQ_URL,
   rabbitMediaQueue: process.env.MEDIA_QUEUE,
   clients: {
